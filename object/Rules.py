8ae12dd702f50a411aa83cf4b20976f814f68635
Oleksii Borysenko <oborysen@e1r3p14.unit.ua>
2018-03-26 23:00:16 +0300

    getH in Rules realization

--- object/Rules.py
+++ object/Rules.py
@@ -12,9 +12,16 @@ class 	Rules:
 		print "State getDistance"
 
 	#  Calculates a heuristic estimate of the distance from the specified state to the final.
-	def getH( self, state ):
-		print "State getH"
-		return 1
+	def getH( self, state, correct_state, digit ):
+		def get_index(matrix, digit):
+			for row,j in enumerate(matrix):
+				for column,l in enumerate(j):
+					if l==digit:
+						return row, column
+		ind_st =  get_index(state, digit)
+		ind_cor = get_index(correct_state, digit)
+		h = abs(ind_st[0] - ind_cor[0]) + abs(ind_st[1] -ind_cor[1])
+		return h
 
 	# Checks whether the state is finished.
 	def isTerminate( self, state ):
--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import numpy as np
 from astaralgo import *
 from fcoefficientsearch import *
 from read import *
+from object.Rules import *
+from object.State import *
 
 resultRead = readFile(sys.argv[1])
 m = convertInMatrix(resultRead)
@@ -20,11 +22,15 @@ def     search_zero_indices(matryx):
 #print np.nanargmin(m.matrix, axis=None)
 
 #row,column = search_zero_indices(m.matrix)
-print check_solve(m.matrix)
+#print check_solve(m.matrix)
 #print fill_matrix(m.matrix)
 matrix_correct = fill_matrix(m.matrix)
+#print m.matrix
 print matrix_correct
 print g_coef_count(matrix_correct, m.matrix)
+digit = 3
+rules = Rules()
+print rules.getH(m.matrix, matrix_correct, digit)
 '''
 m.move_right(row,column)
 print m.matrix
